139a1db4faa6679ab88ff0c5cd85a25c5c4ec99d
Felix Sun <sunfmin@gmail.com>
2019-04-30 17:26:55 +0800

    Add if component

---
+++ html/if.go
@@ -0,0 +1,17 @@
+package html
+
+import (
+	ui "github.com/sunfmin/pagui"
+)
+
+func If(v bool, yes ui.HTMLComponent, no ui.HTMLComponent) (r ui.HTMLComponent) {
+	return ui.ComponentFunc(func(ctx *ui.EventContext) (r []byte, err error) {
+		if v && yes != nil {
+			return yes.MarshalHTML(ctx)
+		}
+		if no != nil {
+			return no.MarshalHTML(ctx)
+		}
+		return
+	})
+}
