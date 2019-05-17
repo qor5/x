7ab004a156e7c8e677a3868bcb50de0fc00470f0
Felix Sun <sunfmin@gmail.com>
2019-05-17 15:28:54 +0800

    Add lazy loader with popover

--- examples/e08_hello_popover/page.go
+++ examples/e08_hello_popover/page.go
@@ -11,14 +11,23 @@ import (
 )
 
 type mystate struct {
-	popoverVisible bool
-	Name           string
-	NameError      string
+	popoverVisible       bool
+	popoverVisibleRemote bool
+	Name                 string
+	NameError            string
 }
 
 func randStr(prefix string) string {
 	rand.Seed(time.Now().UnixNano())
 	return fmt.Sprintf("%s: %d", prefix, rand.Int31n(100))
+}
+
+func overlay(s *mystate, ctx *ui.EventContext) HTMLComponent {
+	return Div(
+		ui.Bind(Input("").Type("text").Value(s.Name)).FieldName("Name"),
+		Label(s.NameError).Style("color:red"),
+		ui.Bind(Button("Update")).OnClick(ctx.Hub, "update", update),
+	)
 }
 
 func HelloPopover(ctx *ui.EventContext) (pr ui.PageResponse, err error) {
@@ -28,13 +37,20 @@ func HelloPopover(ctx *ui.EventContext) (pr ui.PageResponse, err error) {
 		H1(s.Name),
 		bo.Popover(
 			A().Text("Edit").Href("#"),
+		).Overlay(overlay(s, ctx)).DefaultVisible(s.popoverVisible),
+
+		bo.Popover(
+			A().Text("Remote Loader").Href("#"),
 		).Overlay(
-			Button("Close").Attr("@click", "parent.close"),
-			ui.Bind(Input("").Type("text").Value(s.Name)).FieldName("Name"),
-			Label(s.NameError).Style("color:red"),
-			ui.Bind(Button("Update")).OnClick(ctx.Hub, "update", update),
-		).DefaultVisible(s.popoverVisible),
+			bo.LazyLoader(ctx.Hub, "remoteOverlay", remoteOverlay).Visible("true"),
+		).DefaultVisible(s.popoverVisibleRemote),
 	)
+	return
+}
+
+func remoteOverlay(ctx *ui.EventContext) (r ui.EventResponse, err error) {
+	s := ctx.StateOrInit(&mystate{}).(*mystate)
+	r.Schema = overlay(s, ctx)
 	return
 }
 
