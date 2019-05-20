11d9527d73ac90aa4f850ebc17f912364fbc1cb5
Felix Sun <sunfmin@gmail.com>
2019-05-20 16:21:14 +0800

    Made popover samples works

--- examples/e08_hello_popover/page.go
+++ examples/e08_hello_popover/page.go
@@ -10,11 +10,16 @@ import (
 	. "github.com/theplant/htmlgo"
 )
 
+type popoverState struct {
+	Visible     bool
+	EditingName string
+	NameError   string
+}
+
 type mystate struct {
-	popoverVisible       bool
-	popoverVisibleRemote bool
-	EditingName          string
-	NameError            string
+	Popover1 *popoverState
+	Popover2 *popoverState
+	Popover3 *popoverState
 }
 
 var globalState = &struct {
@@ -26,26 +31,37 @@ func randStr(prefix string) string {
 	return fmt.Sprintf("%s: %d", prefix, rand.Int31n(100))
 }
 
-func overlay(s *mystate, ctx *ui.EventContext) HTMLComponent {
+func overlay(s *popoverState, ctx *ui.EventContext, subState string) HTMLComponent {
 	return Div(
-		ui.Bind(Input("").Type("text").Value(s.EditingName)).FieldName("EditingName"),
+		ui.Bind(Input("").Type("text").Value(s.EditingName)).FieldName(fmt.Sprintf("%s.EditingName", subState)),
 		Label(s.NameError).Style("color:red"),
-		ui.Bind(Button("Update")).OnClick(ctx.Hub, "update", update),
+		ui.Bind(Button("Update")).OnClick(ctx.Hub, "update", update, subState),
 	).Style("padding: 20px; background-color: white;")
 }
 
 func HelloPopover(ctx *ui.EventContext) (pr ui.PageResponse, err error) {
-	s := ctx.StateOrInit(&mystate{EditingName: globalState.Name}).(*mystate)
+	s := ctx.StateOrInit(&mystate{
+		Popover1: &popoverState{
+			EditingName: globalState.Name,
+		},
+		Popover2: &popoverState{
+			EditingName: globalState.Name,
+		},
+		Popover3: &popoverState{
+			EditingName: globalState.Name,
+		},
+	}).(*mystate)
 
 	pr.Schema = Div(
 		H1(globalState.Name),
 
 		H2("Default"),
 		bo.Popover(
-			overlay(s, ctx),
+			overlay(s.Popover1, ctx, "Popover1"),
 		).TriggerElement(
 			A().Text("Edit").Href("#"),
-		).DefaultVisible(s.popoverVisible).Placement("right"),
+		).DefaultVisible(s.Popover1.Visible).Placement("right"),
+
 		H2("Load from remote"),
 		bo.Popover(
 			ui.LazyLoader(ctx.Hub, "remoteOverlay", remoteOverlay).
@@ -53,14 +69,14 @@ func HelloPopover(ctx *ui.EventContext) (pr ui.PageResponse, err error) {
 				ParentForceUpdateAfterLoaded(),
 		).TriggerElement(
 			A().Text("Remote Loader").Href("#"),
-		).DefaultVisible(s.popoverVisibleRemote).Placement("right"),
+		).DefaultVisible(s.Popover2.Visible).Placement("right"),
 
 		H2("Load when mouse over"),
 		bo.Popover(
-			overlay(s, ctx),
+			overlay(s.Popover3, ctx, "Popover3"),
 		).TriggerElement(
 			A().Text("Mouseover").Href("#"),
-		).DefaultVisible(s.popoverVisible).
+		).DefaultVisible(s.Popover3.Visible).
 			Trigger("hover").Placement("bottom"),
 	)
 	return
@@ -68,21 +84,21 @@ func HelloPopover(ctx *ui.EventContext) (pr ui.PageResponse, err error) {
 
 func remoteOverlay(ctx *ui.EventContext) (r ui.EventResponse, err error) {
 	s := ctx.State.(*mystate)
-	r.Schema = overlay(s, ctx)
+	r.Schema = overlay(s.Popover2, ctx, "Popover2")
 	return
 }
 
 func update(ctx *ui.EventContext) (r ui.EventResponse, err error) {
 	r.Reload = true
-	s := ctx.State.(*mystate)
+	subState := ctx.Event.Params[0]
+	s := ctx.SubStateOrInit(subState, &popoverState{}).(*popoverState)
 	if len(s.EditingName) < 10 {
 		s.NameError = "name is too short"
-		s.popoverVisible = true
+		s.Visible = true
 	} else {
 		globalState.Name = s.EditingName
 		s.NameError = ""
-		s.popoverVisible = false
+		s.Visible = false
 	}
-
 	return
 }
