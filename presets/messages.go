15e71109b5890565f6e6d6d8b79da2ac413ca4fc
Felix Sun <sunfmin@gmail.com>
2021-09-05 20:08:12 +0800

    Move i18n template as separate fields

--- presets/messages.go
+++ presets/messages.go
@@ -5,49 +5,56 @@ import (
 )
 
 type Messages struct {
-	SuccessfullyUpdated    string
-	New                    string
-	Update                 string
-	Delete                 string
-	Edit                   string
-	OK                     string
-	Cancel                 string
-	Create                 string
-	DeleteConfirmationText func(id string) string
-	CreatingObjectTitle    func(label string) string
-	EditingObjectTitle     func(label string, name string) string
-	ListingObjectTitle     func(label string) string
-	DetailingObjectTitle   func(label string, name string) string
+	SuccessfullyUpdated            string
+	New                            string
+	Update                         string
+	Delete                         string
+	Edit                           string
+	OK                             string
+	Cancel                         string
+	Create                         string
+	DeleteConfirmationTextTemplate string
+	CreatingObjectTitleTemplate    string
+	EditingObjectTitleTemplate     string
+	ListingObjectTitleTemplate     string
+	DetailingObjectTitleTemplate   string
+}
+
+func (msgr *Messages) DeleteConfirmationText(id string) string {
+	return strings.NewReplacer("{id}", id).
+		Replace(msgr.DeleteConfirmationTextTemplate)
+}
+
+func (msgr *Messages) CreatingObjectTitle(modelName string) string {
+	return strings.NewReplacer("{modelName}", modelName).
+		Replace(msgr.CreatingObjectTitleTemplate)
+}
+
+func (msgr *Messages) EditingObjectTitle(label string, name string) string {
+	return strings.NewReplacer("{id}", name, "{modelName}", label).
+		Replace(msgr.EditingObjectTitleTemplate)
+}
+func (msgr *Messages) ListingObjectTitle(label string) string {
+	return strings.NewReplacer("{modelName}", label).
+		Replace(msgr.ListingObjectTitleTemplate)
+}
+func (msgr *Messages) DetailingObjectTitle(label string, name string) string {
+	return strings.NewReplacer("{id}", name, "{modelName}", label).
+		Replace(msgr.DetailingObjectTitleTemplate)
 }
 
 var Messages_en_US = &Messages{
-	SuccessfullyUpdated: "Successfully Updated",
-	EditingObjectTitle: func(label string, name string) string {
-		return strings.NewReplacer("{id}", name, "{modelName}", label).
-			Replace("Editing {modelName} {id}")
-	},
-	CreatingObjectTitle: func(label string) string {
-		return strings.NewReplacer("{modelName}", label).
-			Replace("New {modelName}")
-	},
-	ListingObjectTitle: func(label string) string {
-		return strings.NewReplacer("{modelName}", label).
-			Replace("Listing {modelName}")
-	},
-	DetailingObjectTitle: func(label string, name string) string {
-		return strings.NewReplacer("{id}", name, "{modelName}", label).
-			Replace("{modelName} {id}")
-	},
-	DeleteConfirmationText: func(id string) string {
-		return strings.NewReplacer("{id}", id).
-			Replace("Are you sure you want to delete object with id: {id}?")
-	},
-
-	New:    "New",
-	Update: "Update",
-	Delete: "Delete",
-	Edit:   "Edit",
-	OK:     "OK",
-	Cancel: "Cancel",
-	Create: "Create",
+	DeleteConfirmationTextTemplate: "Are you sure you want to delete object with id: {id}?",
+	CreatingObjectTitleTemplate:    "New {modelName}",
+	EditingObjectTitleTemplate:     "Editing {modelName} {id}",
+	ListingObjectTitleTemplate:     "Listing {modelName}",
+	DetailingObjectTitleTemplate:   "{modelName} {id}",
+	SuccessfullyUpdated:            "Successfully Updated",
+	New:                            "New",
+	Update:                         "Update",
+	Delete:                         "Delete",
+	Edit:                           "Edit",
+	OK:                             "OK",
+	Cancel:                         "Cancel",
+	Create:                         "Create",
 }
