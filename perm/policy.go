b3cadbed3ee7340f659fb6e4f852ae04ca6ec092
Azuma Fan <azuma@theplant.jp>
2021-09-17 17:05:48 +0800

    fix test

---
+++ perm/policy.go
@@ -0,0 +1,57 @@
+package perm
+
+import (
+	"strings"
+
+	"github.com/ory/ladon"
+)
+
+type PolicyBuilder struct {
+	policy *ladon.DefaultPolicy
+	module string
+}
+
+func NewPolicy() *PolicyBuilder {
+	return &PolicyBuilder{
+		policy: &ladon.DefaultPolicy{},
+	}
+}
+
+func (b *PolicyBuilder) Module(module string) (r *PolicyBuilder) {
+	b.module = module
+	return b
+}
+
+func (b *PolicyBuilder) They(subjects ...string) (r *PolicyBuilder) {
+	b.policy.Subjects = subjects
+	return b
+}
+
+func (b *PolicyBuilder) Are(effect string) (r *PolicyBuilder) {
+	b.policy.Effect = effect
+	return b
+}
+
+func (b *PolicyBuilder) ToDo(actions ...string) (r *PolicyBuilder) {
+	b.policy.Actions = actions
+	return b
+}
+
+func (b *PolicyBuilder) On(resources ...string) (r *PolicyBuilder) {
+	if b.module == "" {
+		b.policy.Resources = append(b.policy.Resources, resources...)
+		return b
+	}
+
+	var newRes []string
+	for _, res := range resources {
+		newRes = append(newRes, strings.Join([]string{b.module, res}, ":"))
+	}
+	b.policy.Resources = append(b.policy.Resources, newRes...)
+	return b
+}
+
+func (b *PolicyBuilder) Given(conditions Conditions) (r *PolicyBuilder) {
+	b.policy.Conditions = conditions
+	return b
+}
