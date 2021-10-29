876c7b1e5c59f26eaa02fe11a6d33aff80805593
Charles Shen <shenzhaoyan@gmail.com>
2021-10-29 16:12:26 +0800

    Expose RunSetterFunc

--- presets/editing.go
+++ presets/editing.go
@@ -259,27 +259,50 @@ func (b *EditingBuilder) defaultUpdate(ctx *web.EventContext) (r web.EventRespon
 		}
 	}
 
-	if usingB.Setter != nil {
-		usingB.Setter(obj, ctx)
+	if err2 := usingB.RunSetterFunc(ctx, &r, obj, newObj); err2.HaveErrors() {
+		return
 	}
 
-	var vErr web.ValidationErrors
-	for _, f := range usingB.fields {
+	if usingB.Validator != nil {
+		if vErr := usingB.Validator(obj, ctx); vErr.HaveErrors() {
+			usingB.UpdateRightDrawerContent(ctx, &r, obj, "", &vErr)
+			return
+		}
+	}
 
-		if b.mb.Info().Verifier().Do(PermUpdate).ObjectOn(obj).SnakeOn(f.name).WithReq(ctx.R).IsAllowed() != nil {
+	err1 := usingB.Saver(obj, id, ctx)
+	if err1 != nil {
+		usingB.UpdateRightDrawerContent(ctx, &r, obj, "", err1)
+		return
+	}
+
+	msgr := MustGetMessages(ctx.R)
+	ctx.Flash = msgr.SuccessfullyUpdated
+
+	r.PushState = web.PushState(nil)
+	r.VarsScript = `vars.rightDrawer = false`
+	return
+}
+
+func (b *EditingBuilder) RunSetterFunc(ctx *web.EventContext, r *web.EventResponse, toObj interface{}, fromObj interface{}) (vErr web.ValidationErrors) {
+	if b.Setter != nil {
+		b.Setter(toObj, ctx)
+	}
+	for _, f := range b.fields {
+		if b.mb.Info().Verifier().Do(PermUpdate).ObjectOn(toObj).SnakeOn(f.name).WithReq(ctx.R).IsAllowed() != nil {
 			continue
 		}
 
 		if f.setterFunc == nil {
-			val, err1 := reflectutils.Get(newObj, f.name)
+			val, err1 := reflectutils.Get(fromObj, f.name)
 			if err1 != nil {
 				continue
 			}
-			_ = reflectutils.Set(obj, f.name, val)
+			_ = reflectutils.Set(toObj, f.name, val)
 			continue
 		}
 
-		err1 := f.setterFunc(obj, &FieldContext{
+		err1 := f.setterFunc(toObj, &FieldContext{
 			ModelInfo: b.mb.Info(),
 			Name:      f.name,
 			Label:     b.getLabel(f.NameLabel),
@@ -290,28 +313,9 @@ func (b *EditingBuilder) defaultUpdate(ctx *web.EventContext) (r web.EventRespon
 	}
 
 	if vErr.HaveErrors() {
-		b.UpdateRightDrawerContent(ctx, &r, obj, "", &vErr)
+		b.UpdateRightDrawerContent(ctx, r, toObj, "", &vErr)
 		return
 	}
-
-	if usingB.Validator != nil {
-		if vErr := usingB.Validator(obj, ctx); vErr.HaveErrors() {
-			b.UpdateRightDrawerContent(ctx, &r, obj, "", &vErr)
-			return
-		}
-	}
-
-	err1 := usingB.Saver(obj, id, ctx)
-	if err1 != nil {
-		b.UpdateRightDrawerContent(ctx, &r, obj, "", err1)
-		return
-	}
-
-	msgr := MustGetMessages(ctx.R)
-	ctx.Flash = msgr.SuccessfullyUpdated
-
-	r.PushState = web.PushState(nil)
-	r.VarsScript = `vars.rightDrawer = false`
 	return
 }
 
