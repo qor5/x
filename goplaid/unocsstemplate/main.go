17ee4e3266b912d66fa48b76cec7ff2ff88e1ecc
Felix Sun <sunfmin@gmail.com>
2022-05-07 13:38:36 +0800

    Polish UnoCSS template

--- goplaid/unocsstemplate/main.go
+++ goplaid/unocsstemplate/main.go
@@ -11,8 +11,6 @@ import (
 	. "github.com/theplant/htmlgo"
 )
 
-const doAction1 = "doAction1"
-
 func Home(ctx *web.EventContext) (r web.PageResponse, err error) {
 	r.Body = Div(
 		Div(
@@ -20,11 +18,13 @@ func Home(ctx *web.EventContext) (r web.PageResponse, err error) {
 			Button("Server side action").Attr("@click",
 				web.Plaid().EventFunc(doAction1).Query("id", "1").Go(),
 			).Class("px-5 py-3 rounded-xl text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-800 active:bg-grey-900 focus:outline-none border-4 border-white focus:border-purple-200 transition-all"),
-			Div(
-				Button("Browser side action").Attr("@click", "locals.count++").
-					Class("px-5 py-3 rounded-xl text-sm font-medium text-indigo-600 bg-white outline-none focus:outline-none m-1 hover:m-0 focus:m-0 border border-indigo-600 hover:border-4 focus:border-4 hover:border-indigo-800 hover:text-indigo-800 focus:border-purple-200 active:border-grey-900 active:text-grey-900 transition-all"),
-				Span("{{locals.count}}"),
-			).Attr("v-init-context:locals", "{count: 0}"),
+			web.Scope(
+				Div(
+					Button("Browser side action").Attr("@click", "locals.count++").
+						Class("px-5 py-3 rounded-xl text-sm font-medium text-indigo-600 bg-white outline-none focus:outline-none m-1 hover:m-0 focus:m-0 border border-indigo-600 hover:border-4 focus:border-4 hover:border-indigo-800 hover:text-indigo-800 focus:border-purple-200 active:border-grey-900 active:text-grey-900 transition-all"),
+					Span("{{locals.count}}"),
+				),
+			).VSlot("{ locals }").Init("{count: 0}"),
 		).Class("p-5 w-80 bg-white rounded-lg shadow-lg hover:shadow-2xl cursor-pointer"),
 	).Class("flex items-center justify-center")
 	return
@@ -34,6 +34,11 @@ func DoAction1(ctx *web.EventContext) (r web.EventResponse, err error) {
 	r.Reload = true
 	return
 }
+
+const doAction1 = "doAction1"
+
+var HomePB = web.Page(Home).
+	EventFuncs(doAction1, DoAction1)
 
 func layout(in web.PageFunc) (out web.PageFunc) {
 	return func(ctx *web.EventContext) (pr web.PageResponse, err error) {
@@ -64,19 +69,16 @@ func layout(in web.PageFunc) (out web.PageFunc) {
 }
 
 func main() {
-	w := web.New()
-
 	mux := http.NewServeMux()
 	mux.Handle("/main.js",
-		w.PacksHandler("text/javascript",
+		web.PacksHandler("text/javascript",
 			web.JSVueComponentsPack(),
 			web.JSComponentsPack()))
 	mux.Handle("/main.css",
-		w.PacksHandler("text/css",
+		web.PacksHandler("text/css",
 			css()))
 
-	mux.Handle("/", w.Page(layout(Home)).
-		EventFuncs(doAction1, DoAction1))
+	mux.Handle("/", HomePB.Wrap(layout))
 
 	port := os.Getenv("PORT")
 	if port == "" {
