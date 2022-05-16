ce5e0a41733f74f814c07fbd6ad36d2831ae5341
Chenyang <41779846+moluuser@users.noreply.github.com>
2022-05-16 13:06:12 +0800

    Keep filter while searching

--- presets/presets.go
+++ presets/presets.go
@@ -800,6 +800,7 @@ func (b *Builder) defaultLayout(in web.PageFunc, cfg *LayoutConfig) (out web.Pag
 							Value(ctx.R.URL.Query().Get("keyword")).
 							Attr("@keyup.enter", web.Plaid().
 								Query("keyword", web.Var("[$event.target.value]")).
+							     	MergeQuery(true).
 								PushState(true).
 								Go()).
 							Attr("@click:clear", web.Plaid().
