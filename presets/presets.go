d686edef4f8ef0f1a5e3ab4c0660af754a12b61c
Azuma Daisuke <azuma@theplant.jp>
2022-08-02 10:45:07 +0800

    export DialogPortalName

--- presets/presets.go
+++ presets/presets.go
@@ -633,7 +633,7 @@ func MustGetMessages(r *http.Request) *Messages {
 
 const RightDrawerPortalName = "presets_RightDrawerPortalName"
 const rightDrawerContentPortalName = "presets_RightDrawerContentPortalName"
-const dialogPortalName = "presets_DialogPortalName"
+const DialogPortalName = "presets_DialogPortalName"
 const dialogContentPortalName = "presets_DialogContentPortalName"
 const NotificationCenterPortalName = "notification-center"
 const defaultConfirmationDialogPortalName = "presets_confirmationDialogPortalName"
@@ -682,7 +682,7 @@ func (b *Builder) dialog(r *web.EventResponse, comp h.HTMLComponent, width strin
 		width = b.rightDrawerWidth
 	}
 	r.UpdatePortals = append(r.UpdatePortals, &web.PortalUpdate{
-		Name: dialogPortalName,
+		Name: DialogPortalName,
 		Body: web.Scope(
 			VDialog(
 				web.Portal(comp).Name(dialogContentPortalName),
@@ -878,7 +878,7 @@ func (b *Builder) defaultLayout(in web.PageFunc, cfg *LayoutConfig) (out web.Pag
 			// ClippedLeft(true),
 
 			web.Portal().Name(RightDrawerPortalName),
-			web.Portal().Name(dialogPortalName),
+			web.Portal().Name(DialogPortalName),
 			web.Portal().Name(deleteConfirmPortalName),
 			web.Portal().Name(defaultConfirmationDialogPortalName),
 
