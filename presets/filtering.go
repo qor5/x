15299d31a9dc414f8a7316893717417065d808c9
Azuma Daisuke <azuma@theplant.jp>
2022-10-11 11:49:53 +0800

    add f_ prefix to filter key to avoid conflict with other url params

--- presets/filtering.go
+++ presets/filtering.go
@@ -1,7 +1,18 @@
 package presets
 
+import (
+	"github.com/goplaid/web"
+	"github.com/goplaid/x/vuetifyx"
+)
+
 func (b *ListingBuilder) FilterDataFunc(v FilterDataFunc) {
-	b.filterDataFunc = v
+	b.filterDataFunc = func(ctx *web.EventContext) vuetifyx.FilterData {
+		fd := v(ctx)
+		for _, k := range fd {
+			k.Key = "f_" + k.Key
+		}
+		return fd
+	}
 }
 
 func (b *ListingBuilder) FilterTabsFunc(v FilterTabsFunc) {
