87c749884b2d287fbb3a94a130bd9c21f78bb2ec
xuxin <xuxinmight@gmail.com>
2022-10-13 15:49:29 +0800

    fix filter tab f_ prefix key

--- presets/filtering.go
+++ presets/filtering.go
@@ -1,11 +1,18 @@
 package presets
 
 import (
+	"net/url"
+
 	"github.com/goplaid/web"
 	"github.com/goplaid/x/vuetifyx"
 )
 
 func (b *ListingBuilder) FilterDataFunc(v FilterDataFunc) {
+	if v == nil {
+		b.filterDataFunc = nil
+		return
+	}
+
 	b.filterDataFunc = func(ctx *web.EventContext) vuetifyx.FilterData {
 		fd := v(ctx)
 		for _, k := range fd {
@@ -16,5 +23,20 @@ func (b *ListingBuilder) FilterDataFunc(v FilterDataFunc) {
 }
 
 func (b *ListingBuilder) FilterTabsFunc(v FilterTabsFunc) {
-	b.filterTabsFunc = v
+	if v == nil {
+		b.filterTabsFunc = nil
+		return
+	}
+
+	b.filterTabsFunc = func(ctx *web.EventContext) []*FilterTab {
+		fts := v(ctx)
+		for _, ft := range fts {
+			newQuery := make(url.Values)
+			for k, _ := range ft.Query {
+				newQuery["f_"+k] = ft.Query[k]
+			}
+			ft.Query = newQuery
+		}
+		return fts
+	}
 }
