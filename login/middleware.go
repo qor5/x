e279758342232e032d28857a65facae7e0a4e48f
molon <3739161+molon@users.noreply.github.com>
2024-05-24 13:36:13 +0800

    fix: redirect to `js.map` path after login

--- login/middleware.go
+++ login/middleware.go
@@ -63,7 +63,7 @@ func (b *Builder) Middleware(cfgs ...MiddlewareConfig) func(next http.Handler) h
 		b.validateTOTPURL:              {},
 	}
 
-	staticFileRe := regexp.MustCompile(`\.(css|js|gif|jpg|jpeg|png|ico|svg|ttf|eot|woff|woff2)$`)
+	staticFileRe := regexp.MustCompile(`\.(css|js|gif|jpg|jpeg|png|ico|svg|ttf|eot|woff|woff2|js\.map)$`)
 
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
