6dbd328d3ea995c9771d2e6c3429a30b932b970b
wen <36833420+zhangshanwen@users.noreply.github.com>
2024-07-23 10:02:47 +0800

    upgrade cropper

--- ui/cropper/cropperjs/src/demo/components/CropperExample.vue
+++ ui/cropper/cropperjs/src/demo/components/CropperExample.vue
@@ -1,14 +1,29 @@
 <script setup lang="ts">
 import Cropper from "@/lib/Cropper.vue";
 import { ref } from "vue";
+
 const value = ref();
+const show = ref();
 const src =
   "https://cdn.pixabay.com/photo/2023/11/23/20/40/ocean-8408693_1280.jpg";
 </script>
 
 <template>
   <p>{{ value }}</p>
-  <cropper v-model="value" :src="src"></cropper>
+  <button @click="show = !show">click me</button>
+  <div v-if="show" class="box">
+    <cropper
+      v-model="value"
+      :src="src"
+      :view-mode="3"
+      :auto-crop-area="1.0"
+    ></cropper>
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.box {
+  height: 400px;
+  width: 400px;
+}
+</style>
