738137ff4df8776fc2f5e3c1dc8e3d06ab06a1bb
molon <3739161+molon@users.noreply.github.com>
2024-07-30 17:59:51 +0800

    datepicker: make datePickerProps available

--- ui/vuetifyx/vuetifyxjs/src/demo/components/DatetimepickerExample.vue
+++ ui/vuetifyx/vuetifyxjs/src/demo/components/DatetimepickerExample.vue
@@ -7,7 +7,7 @@ const value = ref('2023-10-01 22:33')
 
 <template>
   <p>{{ value }}</p>
-  <datetimepicker v-model="value" />
+  <datetimepicker v-model="value" :date-picker-props="{ min: '2016-06-15', max: '2028-03-20' }" />
 </template>
 
 <style scoped></style>
