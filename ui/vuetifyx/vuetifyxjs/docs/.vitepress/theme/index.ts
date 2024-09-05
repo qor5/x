85cb7286b6cd1d2d9fd1f2d7bb6a38137ea1f449
Daniel <contact@danni.cool>
2024-09-05 18:42:31 +0800

    chore: modifiy theme

--- ui/vuetifyx/vuetifyxjs/docs/.vitepress/theme/index.ts
+++ ui/vuetifyx/vuetifyxjs/docs/.vitepress/theme/index.ts
@@ -1,9 +1,15 @@
 // theme/index.ts
 import DefaultTheme from 'vitepress/theme';
+import theme from './theme'
 import { registerComponents } from './register-components.ts'
+import { h } from 'vue'
+import myLayout from './myLayout'
 
 export default {
-    ...DefaultTheme,
+    ...theme,
+    Layout: () => {
+      return h(myLayout)
+    },
     enhanceApp({app}) {
       registerComponents(app)
     },
