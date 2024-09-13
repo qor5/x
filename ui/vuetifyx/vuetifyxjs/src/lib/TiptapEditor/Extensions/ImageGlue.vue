2239a6ccb7063f3511f9d0f699a1051279ce9b47
molon <3739161+molon@users.noreply.github.com>
2024-09-13 19:26:34 +0800

    tiptap: remove unimportant comments

--- ui/vuetifyx/vuetifyxjs/src/lib/TiptapEditor/Extensions/ImageGlue.vue
+++ ui/vuetifyx/vuetifyxjs/src/lib/TiptapEditor/Extensions/ImageGlue.vue
@@ -15,13 +15,11 @@ const props = withDefaults(defineProps<Props>(), {
 
 const elementRef = ref<HTMLElement | null>(null)
 
-// 定义 click 函数的参数类型
 interface ClickParams {
   editor: Editor
   value: any
 }
 
-// 修改 inject 的类型参数，明确指定 editor 和 value 的类型
 const click = inject<(params: ClickParams) => void>('__imageGlueClick__')
 
 const emitClick = () => {
