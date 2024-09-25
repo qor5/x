3f31c380565376a932c036cc9ea9e7633c0417dd
danni-cool <danni-cool@users.noreply.github.com>
2024-09-25 07:25:17 +0000

    deploy: d9262ada53f51be11dbc5b8284174d0da0fc3719

--- assets/Components_ScrollIframe_index.md.CPo3dFeL.lean.js
+++ assets/Components_ScrollIframe_index.md.CPo3dFeL.lean.js
@@ -1,4 +1,4 @@
-import{e as n}from"./chunks/theme.2kjRJfwq.js";import{f as B,d as C,a8 as F,R as r,S as d,l,U as h,O as i,F as c,W as s}from"./chunks/framework.zpxlqNnZ.js";const o=B({__name:"virtual:index.md.222172d6",setup(A){const k=C(),E=C(`
+import{e as n}from"./chunks/theme.BNZ6lCDj.js";import{f as B,d as C,a8 as F,R as r,S as d,l,U as h,O as i,F as c,W as s}from"./chunks/framework.zpxlqNnZ.js";const o=B({__name:"virtual:index.md.222172d6",setup(A){const k=C(),E=C(`
 <!DOCTYPE html>
 <body>
    <div id="app">
