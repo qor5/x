4bbbba08e657107913048cbb192a12de41a572bd
Daniel <contact@danni.cool>
2024-11-27 15:54:42 +0800

    chore: update the ci

--- ui/vuetifyx/vuetifyxjs/src/lib/Common/VXLabel.vue
+++ ui/vuetifyx/vuetifyxjs/src/lib/Common/VXLabel.vue
@@ -77,7 +77,7 @@ const props = defineProps({
     type: Boolean,
     default: false
   },
-  textProps: String //TODO:
+  textProps2: String //TODO:
 })
 const { icon: _, ...propsWithoutIcon } = props
 const hasDefaultSlot = !!slots.default
