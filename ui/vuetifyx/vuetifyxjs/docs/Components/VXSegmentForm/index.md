462b5d70003f33aff1706a96fabd4298e9931897
Daniel <contact@danni.cool>
2025-03-12 10:44:36 +0800

    feat: init commit for segment form

---
+++ ui/vuetifyx/vuetifyxjs/docs/Components/VXSegmentForm/index.md
@@ -0,0 +1,16 @@
+# VXSegmantForm
+
+这是一个基本示例，你可以用 `markdown` 语法 和 `vue3`、`vuetify` 在此处写任何组件代码
+
+:::demo
+
+```vue
+<script setup lang="ts"></script>
+<template>
+  <vx-segment-form> </vx-segment-form>
+</template>
+```
+
+<style scoped></style>
+
+:::
---
+++ ui/vuetifyx/vuetifyxjs/src/lib/Form/VXSegmentForm/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="vx-segment-form">
+    <div class="vx-condition-btn-group">
+      <button>And</button>
+      <button>Or</button>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="scss" scoped>
+.vx-segment-form {
+  .vx-condition-btn-group {
+    display: flex;
+    gap: 10px;
+  }
+}
+</style>
