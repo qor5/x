daf65ac15085b744f1d32322892a5a85b59f3975
Daniel <contact@danni.cool>
2025-06-04 12:56:12 +0800

    chore: update project configuration for Storybook integration
    - Added Storybook commands to package.json for development and build.
    - Updated dependencies related to Storybook in package.json and pnpm-lock.yaml.
    - Enhanced .gitignore to exclude Storybook logs and static files.

---
+++ ui/vuetifyx/vuetifyxjs/.storybook/main.ts
@@ -0,0 +1,17 @@
+import type { StorybookConfig } from '@storybook/vue3-vite';
+
+const config: StorybookConfig = {
+  "stories": [
+    "../src/**/*.mdx",
+    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-onboarding",
+    "@storybook/addon-docs"
+  ],
+  "framework": {
+    "name": "@storybook/vue3-vite",
+    "options": {}
+  }
+};
+export default config;
