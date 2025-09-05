a1c6f89956517936b9c8275062aa30e6e0bf35b3
molon <3739161+molon@users.noreply.github.com>
2025-09-05 15:08:35 +0800

    Fix error handling in Migrate function

--- gobusx/migrate.go
+++ gobusx/migrate.go
@@ -15,5 +15,9 @@ func Migrate(ctx context.Context, db DB) (*migrator, error) {
 	if err != nil {
 		return nil, errors.Wrap(err, "failed to get database connection")
 	}
-	return &migrator{}, pgbus.Migrate(ctx, sqlDB)
+	err = pgbus.Migrate(ctx, sqlDB)
+	if err != nil {
+		return nil, err
+	}
+	return &migrator{}, nil
 }
