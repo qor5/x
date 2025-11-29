f8184d3f46ae9abc73acf5090793c446c18d3e66
Eider Oliveira <eider.oliveira@gmail.com>
2025-11-29 09:13:25 -0300

    feat(timeline): add VXTimeline component and integrate with VuetifyX
    - Introduced VXTimeline component with customizable properties such as line thickness, colors, and alignment.
    - Updated VuetifyX plugin to register the new VXTimeline component.
    - Bumped pnpm version in package.json to 10.23.0.

---
+++ ui/vuetifyx/vuetifyxjs/src/lib/Timeline/VXTimeline.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="vx-timeline-wrap" :class="{ 'vx-timeline-sinuous': sinuous }" v-bind="rootAttrs">
+    <v-timeline v-bind="combinedProps">
+      <template v-if="!isDefaultSlotReallyEmpty" #default>
+        <slot />
+      </template>
+    </v-timeline>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { defineEmits, computed, useSlots, defineOptions } from 'vue'
+import { useFilteredAttrs } from '@/lib/composables/useFilteredAttrs'
+
+const { filteredAttrs, rootAttrs } = useFilteredAttrs()
+const slots = useSlots()
+const emit = defineEmits(['update:modelValue'])
+const props = defineProps({
+  sinuous: Boolean
+})
+
+const defaultOptions = computed(() => {
+  return {
+    // Default options if any
+  }
+})
+
+const isDefaultSlotReallyEmpty = computed(() => {
+  /* @ts-ignore */
+  return !slots.default || !slots.default().length
+})
+
+// bugfix: bind event will auto bind to rootElement, and result in trigger twice
+defineOptions({
+  inheritAttrs: false
+})
+
+const combinedProps = computed(() => ({
+  ...defaultOptions.value,
+  ...filteredAttrs.value // passthrough the props that defined by vuetify
+}))
+</script>
+
+<style lang="scss" scoped>
+.vx-timeline-wrap {
+  &.vx-timeline-sinuous {
+    :deep(.v-timeline-divider__before),
+    :deep(.v-timeline-divider__after) {
+      background-color: transparent !important;
+      width: 20px;
+      background-image: url("data:image/svg+xml,%3Csvg width='16' height='130' viewBox='0 0 12 100' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M6 0 Q 12 25 6 50 T 6 100' fill='none' stroke='%23ccc' stroke-width='2'/%3E%3C/svg%3E");
+      background-repeat: repeat-y;
+    }
+
+    :deep(.v-timeline-divider__before) {
+      background-position: bottom center;
+    }
+
+    :deep(.v-timeline-divider__after) {
+      background-position: top center;
+    }
+  }
+}
+</style>
---
+++ ui/vuetifyx/vx-timeline.go
@@ -0,0 +1,152 @@
+package vuetifyx
+
+import (
+	"context"
+	"fmt"
+
+	h "github.com/theplant/htmlgo"
+)
+
+type VXTimelineBuilder struct {
+	tag *h.HTMLTagBuilder
+}
+
+func VXTimeline(children ...h.HTMLComponent) (r *VXTimelineBuilder) {
+	r = &VXTimelineBuilder{
+		tag: h.Tag("vx-timeline").Children(children...),
+	}
+	return
+}
+
+func (b *VXTimelineBuilder) Justify(v string) (r *VXTimelineBuilder) {
+	b.tag.Attr("justify", v)
+	return b
+}
+
+func (b *VXTimelineBuilder) LineThickness(v interface{}) (r *VXTimelineBuilder) {
+	b.tag.Attr(":line-thickness", h.JSONString(v))
+	return b
+}
+
+func (b *VXTimelineBuilder) LineColor(v string) (r *VXTimelineBuilder) {
+	b.tag.Attr("line-color", v)
+	return b
+}
+
+func (b *VXTimelineBuilder) DotColor(v string) (r *VXTimelineBuilder) {
+	b.tag.Attr("dot-color", v)
+	return b
+}
+
+func (b *VXTimelineBuilder) FillDot(v bool) (r *VXTimelineBuilder) {
+	b.tag.Attr(":fill-dot", fmt.Sprint(v))
+	return b
+}
+
+func (b *VXTimelineBuilder) HideOpposite(v bool) (r *VXTimelineBuilder) {
+	b.tag.Attr(":hide-opposite", fmt.Sprint(v))
+	return b
+}
+
+func (b *VXTimelineBuilder) IconColor(v string) (r *VXTimelineBuilder) {
+	b.tag.Attr("icon-color", v)
+	return b
+}
+
+func (b *VXTimelineBuilder) LineInset(v interface{}) (r *VXTimelineBuilder) {
+	b.tag.Attr(":line-inset", h.JSONString(v))
+	return b
+}
+
+func (b *VXTimelineBuilder) Size(v interface{}) (r *VXTimelineBuilder) {
+	b.tag.Attr(":size", h.JSONString(v))
+	return b
+}
+
+func (b *VXTimelineBuilder) Tag(v string) (r *VXTimelineBuilder) {
+	b.tag.Attr("tag", v)
+	return b
+}
+
+func (b *VXTimelineBuilder) Density(v interface{}) (r *VXTimelineBuilder) {
+	b.tag.Attr(":density", h.JSONString(v))
+	return b
+}
+
+func (b *VXTimelineBuilder) Theme(v string) (r *VXTimelineBuilder) {
+	b.tag.Attr("theme", v)
+	return b
+}
+
+func (b *VXTimelineBuilder) Align(v interface{}) (r *VXTimelineBuilder) {
+	b.tag.Attr(":align", h.JSONString(v))
+	return b
+}
+
+func (b *VXTimelineBuilder) Direction(v interface{}) (r *VXTimelineBuilder) {
+	b.tag.Attr(":direction", h.JSONString(v))
+	return b
+}
+
+func (b *VXTimelineBuilder) Side(v interface{}) (r *VXTimelineBuilder) {
+	b.tag.Attr(":side", h.JSONString(v))
+	return b
+}
+
+func (b *VXTimelineBuilder) TruncateLine(v interface{}) (r *VXTimelineBuilder) {
+	b.tag.Attr(":truncate-line", h.JSONString(v))
+	return b
+}
+
+func (b *VXTimelineBuilder) SetAttr(k string, v interface{}) {
+	b.tag.SetAttr(k, v)
+}
+
+func (b *VXTimelineBuilder) Attr(vs ...interface{}) (r *VXTimelineBuilder) {
+	b.tag.Attr(vs...)
+	return b
+}
+
+func (b *VXTimelineBuilder) Children(children ...h.HTMLComponent) (r *VXTimelineBuilder) {
+	b.tag.Children(children...)
+	return b
+}
+
+func (b *VXTimelineBuilder) AppendChildren(children ...h.HTMLComponent) (r *VXTimelineBuilder) {
+	b.tag.AppendChildren(children...)
+	return b
+}
+
+func (b *VXTimelineBuilder) PrependChildren(children ...h.HTMLComponent) (r *VXTimelineBuilder) {
+	b.tag.PrependChildren(children...)
+	return b
+}
+
+func (b *VXTimelineBuilder) Class(names ...string) (r *VXTimelineBuilder) {
+	b.tag.Class(names...)
+	return b
+}
+
+func (b *VXTimelineBuilder) ClassIf(name string, add bool) (r *VXTimelineBuilder) {
+	b.tag.ClassIf(name, add)
+	return b
+}
+
+func (b *VXTimelineBuilder) Sinuous(v bool) (r *VXTimelineBuilder) {
+	b.tag.Attr(":sinuous", fmt.Sprint(v))
+	return b
+}
+
+func (b *VXTimelineBuilder) On(name string, value string) (r *VXTimelineBuilder) {
+	b.tag.Attr(fmt.Sprintf("v-on:%s", name), value)
+	return b
+}
+
+func (b *VXTimelineBuilder) Bind(name string, value string) (r *VXTimelineBuilder) {
+	b.tag.Attr(fmt.Sprintf("v-bind:%s", name), value)
+	return b
+}
+
+func (b *VXTimelineBuilder) MarshalHTML(ctx context.Context) (r []byte, err error) {
+	return b.tag.MarshalHTML(ctx)
+}
